4363d826fc09e9eb39b7ecb349b4369c7edff684
Ervis Trupja <ervistrupja@outlook.com>
2021-06-24 11:05:47 +0200

    Add project files.

---
+++ SchoolApp.API/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SchoolApp.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : ControllerBase
+    {
+        public HomeController()
+        {
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Welcome to HomeController");
+        }
+    }
+}
---
+++ SchoolApp.API/Controllers/ManagementController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SchoolApp.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ManagementController : ControllerBase
+    {
+        public ManagementController()
+        {
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Welcome to ManagementController");
+        }
+    }
+}
